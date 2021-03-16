1c6f4c5bec440dbcb99504e41b3e2c23a1a128ea
momo <wdudtlrw78@gmail.com>
2021-03-17 00:02:52 +0900

    배열 항목 추가

--- basic/App.js
+++ basic/App.js
@@ -1,12 +1,63 @@
 /** @format */
 
-import React from 'react';
-import Hello from './Hello';
+import React, { useRef, useState } from 'react';
 import InputSample from './InputSample';
-import Wrapper from './Wrapper';
 import UserList from './UserList';
+import CreateUser from './CreateUser';
+
 const App = () => {
-  return <UserList />;
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: '',
+  });
+  const { username, email } = inputs;
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value,
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'momo1',
+      email: 'momo1@gmail.com',
+    },
+    {
+      id: 2,
+      username: 'momo2',
+      email: 'momo2@gmail.com',
+    },
+    {
+      id: 3,
+      username: 'momo3',
+      email: 'momo3@gmail.com',
+    },
+  ]);
+
+  const nextId = useRef(4);
+
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email,
+    };
+    setUsers([...users, user]);
+    setInputs({
+      username: '',
+      email: '',
+    });
+    nextId.current += 1;
+  };
+
+  return (
+    <>
+      <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate} />
+      <UserList users={users} />
+    </>
+  );
 };
 
 export default App;
--- basic/UserList.jsx
+++ basic/UserList.jsx
@@ -8,25 +8,7 @@ function User({ user }) {
   );
 }
 
-function UserList() {
-  const users = [
-    {
-      id: 1,
-      username: 'momo1',
-      email: 'momo1@gmail.com',
-    },
-    {
-      id: 2,
-      username: 'momo2',
-      email: 'momo2@gmail.com',
-    },
-    {
-      id: 3,
-      username: 'momo3',
-      email: 'momo3@gmail.com',
-    },
-  ];
-
+function UserList({ users }) {
   return (
     <div>
       {users.map((user) => (
