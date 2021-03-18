b209da05090527bcad5e29152fa2fe7249336a25
momo <wdudtlrw78@gmail.com>
2021-03-18 13:36:46 +0900

    Learn useReducer

--- basic/client.js
+++ basic/client.js
@@ -3,5 +3,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Counter from './Counter';
 
 ReactDOM.render(<App />, document.querySelector('#app'));
