ed31aa6f9d4762d93540c61ea41ec04f5c9ce185
momo <wdudtlrw78@gmail.com>
2021-03-21 20:03:02 +0900

    Learn scss of size, color, outline, fullWidth props config

---
+++ scss-tutorial/styling-with-sass/src/App.js
@@ -0,0 +1,59 @@
+/** @format */
+
+import React from 'react';
+import './App.scss';
+import Button from './components/Button';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="buttons">
+        <Button size="large">BUTTON</Button>
+        <Button>BUTTON</Button>
+        <Button size="small">BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button color="gray" size="large">
+          BUTTON
+        </Button>
+        <Button color="gray">BUTTON</Button>
+        <Button color="gray" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button color="pink" size="large">
+          BUTTON
+        </Button>
+        <Button color="pink">BUTTON</Button>
+        <Button color="pink" size="small">
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" outline>
+          BUTTON
+        </Button>
+        <Button color="gray" outline>
+          BUTTON
+        </Button>
+        <Button color="pink" size="small" outline>
+          BUTTON
+        </Button>
+      </div>
+      <div className="buttons">
+        <Button size="large" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" color="gray" fullWidth>
+          BUTTON
+        </Button>
+        <Button size="large" color="pink" fullWidth>
+          BUTTON
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
