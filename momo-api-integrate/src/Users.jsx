8c699dcf8d379bbce6ae62d253ffae4034c2fc3a
momo <wdudtlrw78@gmail.com>
2021-03-24 23:49:42 +0900

    Review momo-api-connection

--- momo-api-integrate/src/Users.jsx
+++ momo-api-integrate/src/Users.jsx
@@ -1,42 +1,33 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
+import { useAsync } from './useAsync';
+import User from './User';
 
-
+async function getUsers() {
+  const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+  return response.data;
+}
 
 function Users() {
-  const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [state, refetch] = useAsync(getUsers, [], true);
+  const [userId, setUserId] = useState(null);
 
-  useEffect(() => {
-    const fetchUsers = async() => {
-      try {
-        setUsers(null);
-        setError(null);
-        setLoading(true);
-
-        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-        setUsers(response.data);  
-      } catch(e) {
-        setError(e);
-      }
-      setLoading(false);
-    }
-    fetchUsers();
-  }, [])
+  const {loading, data:users, error} = state;
 
   if (loading) return <div>로딩중..</div>
   if (error) return <div>에러가 발생했습니다</div>
-  if (!users) return null 
+  if (!users) return <button onClick={refetch}>불러오기</button> 
   return (
     <>
       <ul>
         {users.map((user) => (
-          <li key={user.id}>
+          <li key={user.id} onClick={() => setUserId(user.id)}>
             {user.username} ({user.name}) ({user.address.city})
           </li>
         ))}
       </ul>
+      <button onClick={refetch}>다시 불러오기</button>
+      {userId && <User id={userId} />}
     </>
   );
 }
