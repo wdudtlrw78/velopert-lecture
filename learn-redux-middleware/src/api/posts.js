abd5129a68dd2983aa23075f899c587a3f2e4534
momo <wdudtlrw78@gmail.com>
2021-03-29 21:23:34 +0900

    Updated json-server

--- learn-redux-middleware/src/api/posts.js
+++ learn-redux-middleware/src/api/posts.js
@@ -1,30 +1,11 @@
-const sleep = n => new Promise(resolve => setTimeout(resolve, n));
-
-// { id, title, body }
-const posts = [
-  {
-    id: 1,
-    title: 'test1',
-    body: 'test1',
-  },
-  {
-    id: 2,
-    title: 'test2',
-    body: 'test2',
-  },
-  {
-    id: 3,
-    title: 'test3',
-    body: 'test3',
-  }
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
 }
 
 export const getPostById = async (id)=> {
-  await sleep(500);
-  return posts.find(post => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 }
