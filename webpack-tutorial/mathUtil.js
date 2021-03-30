8954ff18648c48c0b66fc5c0e287e0c442fc2bc9
momo <wdudtlrw78@gmail.com>
2021-03-30 19:55:20 +0900

    Module 도형의 넓이 예제

--- webpack-tutorial/mathUtil.js
+++ webpack-tutorial/mathUtil.js
@@ -1,16 +1,18 @@
 const PI = 3.14;
 const getCircleArea = r => r * PI;
+const getSquareArea = d => d * d;
 
 // commonJS
 module.exports = {
   PI,
-  getCircleArea
+  getCircleArea,
+  getSquareArea
 }
-exports.PI = PI;
-exports.getCircleArea = getCircleArea;
+// exports.PI = PI;
+// exports.getCircleArea = getCircleArea;
 
 // ESM
-export {
-  PI,
-  getCircleArea,
-}
+// export {
+//   PI,
+//   getCircleArea,
+// }
---
+++ webpack-tutorial/readline.js
@@ -0,0 +1,33 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+})
+
+const { getCircleArea, getSquareArea } = require('./mathUtil');
+const { logInput, logResult, logFigureError } = require('./log');
+
+rl.question(
+  '넓이를 구하고자 하는 도형의 종류를 입력해주세요. ( 정사각형, 원 ) : ',
+  figure => {
+    console.log(`선택된 도형: ${figure}`);
+    
+    switch(figure) {
+      case '정사각형':
+        rl.question('변의 길이를 입력해주세요 : ', input => {
+          console.log(logInput(input));
+          console.log(logResult(figure, getSquareArea(input)));
+          rl.close();
+        })
+      case '원':
+        rl.question('반지름의 길이를 빈력해주세요 :', input => {
+          console.log(logInput(input));
+          console.log(logResult(figure, getCircleArea(input)));
+          rl.close();
+        });
+        break;
+        default:
+          console.log(logFigureError);
+          rl.close();
+    }
+})
