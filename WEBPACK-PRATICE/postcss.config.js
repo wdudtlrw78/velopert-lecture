14a041ab4a03e8e1e232e64b563cf9b0428542e7
momo <wdudtlrw78@gmail.com>
2021-04-02 21:19:19 +0900

    Updated vender prefixer

--- WEBPACK-PRATICE/postcss.config.js
+++ WEBPACK-PRATICE/postcss.config.js
@@ -1,5 +1,4 @@
 module.export = {
-  plugins: [
-    require('autoprefixer')
-  ]
-}
+  plugins: [require('autoprefixer')],
+  //벤더 프리픽스(vendor prefix)란 주요 웹 브라우저 공급자가 새로운 실험적인 기능을 제공할 때 이전 버전의 웹 브라우저에 그 사실을 알려주기 위해 사용하는 접두사(prefix)를 의미합니다. 즉 아직 CSS 권고안에 포함되지 못한 기능이나, CSS 권고안에는 포함되어 있지만 아직 완벽하게 제정된 상태가 아닌 기능을 사용하고자 할 때 벤더 프리픽스를 사용하게 됩니다.
+};
