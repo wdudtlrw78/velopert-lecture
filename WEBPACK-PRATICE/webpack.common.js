c21544b5ac247d362b0e169ad980986457b73379
momo <wdudtlrw78@gmail.com>
2021-04-02 21:08:00 +0900

    Learn sass loader

--- WEBPACK-PRATICE/webpack.common.js
+++ WEBPACK-PRATICE/webpack.common.js
@@ -11,46 +11,68 @@ module.exports = {
   entry: './src/index.js',
   module: {
     rules: [
+      // filename.module.scss => css modules, // filename.scss => global
       {
-        test: /\.css$/i,
-        use: [
-          // {
-          //   loader: 'style-loader',
-          //   options: {
-          //     injectType: 'singletonStyleTag',
-          //   },
-          // }, 충돌로 인해 주석 (MiniCssExtractPlugin)
+        test: /\.s?css$/,
+        oneOf: [
           {
-            loader: MiniCssExtractPlugin.loader,
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              modules: true,
-            },
-          },
+            test: /\.module\.s?css$/,
+            use: [
+              {
+                loader: MiniCssExtractPlugin.loader,
+              },
+              {
+                loader: 'css-loader',
+                options: {
+                  modules: true,
+                },
+              },
+              'sass-loader'
+            ],
+          }, {
+            use: [
+              MiniCssExtractPlugin.loader,
+              'css-loader',
+              'sass-loader',
+            ]
+          }
         ],
       },
       {
         test: /\.hbs$/,
         use: ['handlebars-loader'],
-      }, {
-        test: /\.(png | jpe?g | gif)$/i,
-        use: [{
-          loader: 'file-loader',
-          options: {
-            name() {
-              if (!isProduction) {
-                return '[path][name].[ext]';
-              }
-              return '[contenthash].[ext]';
-            },
-            publicPath: 'assets/',
-            outputPath: 'assets/',
-          }
-        }] 
       },
-      
+      {
+        test: /\.(png | jpe?g | gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name() {
+                if (!isProduction) {
+                  return '[path][name].[ext]';
+                }
+                return '[contenthash].[ext]';
+              },
+              publicPath: 'assets/',
+              outputPath: 'assets/',
+            },
+          },
+        ],
+      },
+      {
+        // data:mediatype;base64, data -> 데이터 url 형태로된 문자열로 반환된다.
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'url-loader', // url - loader 이점 : 리소스 요청수를 줄일 수 있다.
+            // 문자열 형태로 변경되어서 바로 문서 내부에 적용되기 때문에 문서의 크기는 커질 수 있으나 이미지가 아주 작은사이즈들을 다룰경우에는 문자열로 변환된 값이 크게 부담이 되진 않기 때문에 문서를 다운받는 시간에 영향을 주진 않고 반면에 리소스 요청 수 가 줄기 때문에 중요한 파일들을 빨리 불러올 수 있다.
+            options: {
+              limit: 8192, // 파일크기의 제한 8kb까지 적용
+            },
+          },
+        ],
+      },
     ],
   },
   plugins: [
@@ -63,16 +85,18 @@ module.exports = {
       meta: {
         viewport: 'width=device-width, initial-scale=1.0',
       },
-      minify: isProduction ? {
-        collapseWhitespace: true,
-        useShortDoctype: true,
-        removeScriptTypeAttributes: true,
-      } : false,
+      minify: isProduction
+        ? {
+            collapseWhitespace: true,
+            useShortDoctype: true,
+            removeScriptTypeAttributes: true,
+          }
+        : false,
     }),
     new CleanWebpackPlugin(),
     new webpack.DefinePlugin({
       IS_PRODUCTION: true,
-    })
+    }),
   ],
   output: {
     filename: '[name].[chunkhash].js',
